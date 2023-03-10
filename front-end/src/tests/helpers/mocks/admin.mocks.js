532b003f2c6ac71b7243734f135a8efae1636528
Guilherme Garcia <garciaguig@gmail.com>
2023-03-10 18:01:19 -0300

    test: Add testes para page Admin (front)

--- front-end/src/tests/helpers/mocks/admin.mocks.js
+++ front-end/src/tests/helpers/mocks/admin.mocks.js
@@ -1,4 +1,21 @@
-const allUsers = [
+const formElements = {
+  name: 'admin_manage__input-name',
+  email: 'admin_manage__input-email',
+  password: 'admin_manage__input-password',
+  role: 'admin_manage__select-role',
+  button: 'admin_manage__button-register',
+  invalidationMessage: 'admin_manage__element-invalid-register',
+};
+
+const tableElements = {
+  number: 'admin_manage__element-user-table-item-number-',
+  name: 'admin_manage__element-user-table-name-',
+  email: 'admin_manage__element-user-table-email-',
+  role: 'admin_manage__element-user-table-role-',
+  rmButton: 'admin_manage__element-user-table-remove-',
+};
+
+const registeredUsers = [
   {
     id: 2,
     name: 'Fulana Pereira',
@@ -13,6 +30,23 @@ const allUsers = [
   },
 ];
 
+const newUser = {
+  id: 4,
+  name: 'Bruce Wayne Batman',
+  email: 'brucewayne@mail.com',
+  password: 'imnotbatman123',
+  role: 'administrator',
+};
+
+const allUsers = [
+  ...registeredUsers,
+  newUser,
+];
+
 export default {
+  formElements,
+  tableElements,
+  registeredUsers,
+  newUser,
   allUsers,
 };
